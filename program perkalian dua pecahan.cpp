fbf4ee57575ff6ab6c15031274d8bdafd30884e8
heryanto1994 <37211934+heryanto1994@users.noreply.github.com>
2018-12-19 09:53:10 +0700

    Add files via upload

---
+++ program perkalian dua pecahan.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int p1,p2,hasil;
+    cout<<"******PROGRAM PERKALIAN DUA PECAHAN******\n";
+    cout<<"=========================================\n";
+    cout<<"\nMasukan Nilai P1   : ";
+    cin>>p1;
+    cout<<"\nMasukan Nilai P2   : ";
+    cin>>p2;
+    cout<<"\n=====HASILNYA=====\n";
+    p1=3/4;
+    p2=2/3;
+    hasil=(3*2)/(4*3);
+    cout<<"\nMaka Hasil dari perkalian dua pecahan tersebut adalah  : "<<hasil<<"\n";
+    return 0;
+}
